f8edfc3cad76f749b427ad56bb34df59109093b9
tmsksfh2012 <tmsksfh2012@naver.com>
2023-01-09 22:51:12 +0900

    2023/01/09 - 폰트 적용 및 diary2 이미지 넣음

--- recordOfMemory/app/src/main/java/com/recordOfMemory/src/main/home/HomeFragment.kt
+++ recordOfMemory/app/src/main/java/com/recordOfMemory/src/main/home/HomeFragment.kt
@@ -5,54 +5,60 @@ import android.util.Log
 import android.view.View
 import com.recordOfMemory.R
 import com.recordOfMemory.config.BaseFragment
+import com.recordOfMemory.databinding.FragmentDiary2Binding
 import com.recordOfMemory.databinding.FragmentHomeBinding
 import com.recordOfMemory.src.main.home.models.PostSignUpRequest
 import com.recordOfMemory.src.main.home.models.SignUpResponse
 import com.recordOfMemory.src.main.home.models.UserResponse
 
-class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::bind, R.layout.fragment_home),
-        HomeFragmentInterface {
+class HomeFragment : BaseFragment<FragmentDiary2Binding>(FragmentDiary2Binding::bind, R.layout.fragment_diary2){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.homeButtonTryGetJwt.setOnClickListener {
-            showLoadingDialog(requireContext())
-            HomeService(this).tryGetUsers()
+        binding.diary2IvList.setOnCheckedChangeListener { buttonView, isChecked ->
+            binding.diary2IvGrid.isChecked = !isChecked
+        }
+        binding.diary2IvGrid.setOnCheckedChangeListener { buttonView, isChecked ->
+            binding.diary2IvList.isChecked = !isChecked
         }
 
-        binding.homeBtnTryPostHttpMethod.setOnClickListener {
-            val email = binding.homeEtId.text.toString()
-            val password = binding.homeEtPw.text.toString()
-            val postRequest = PostSignUpRequest(email = email, password = password,
-                    confirmPassword = password, nickname = "test", phoneNumber = "010-0000-0000")
-            showLoadingDialog(requireContext())
-            HomeService(this).tryPostSignUp(postRequest)
-        }
-    }
-
-    override fun onGetUserSuccess(response: UserResponse) {
-        dismissLoadingDialog()
-        for (User in response.result) {
-            Log.d("HomeFragment", User.toString())
-        }
-        binding.homeButtonTryGetJwt.text = response.message
-        showCustomToast("Get JWT 성공")
-    }
-
-    override fun onGetUserFailure(message: String) {
-        dismissLoadingDialog()
-        showCustomToast("오류 : $message")
-    }
-
-    override fun onPostSignUpSuccess(response: SignUpResponse) {
-        dismissLoadingDialog()
-        binding.homeBtnTryPostHttpMethod.text = response.message
-        response.message?.let { showCustomToast(it) }
-    }
-
-    override fun onPostSignUpFailure(message: String) {
-        dismissLoadingDialog()
-        showCustomToast("오류 : $message")
+//        binding.homeButtonTryGetJwt.setOnClickListener {
+//            showLoadingDialog(requireContext())
+//            HomeService(this).tryGetUsers()
+//        }
+//
+//        binding.homeBtnTryPostHttpMethod.setOnClickListener {
+//            val email = binding.homeEtId.text.toString()
+//            val password = binding.homeEtPw.text.toString()
+//            val postRequest = PostSignUpRequest(email = email, password = password,
+//                    confirmPassword = password, nickname = "test", phoneNumber = "010-0000-0000")
+//            showLoadingDialog(requireContext())
+//            HomeService(this).tryPostSignUp(postRequest)
+//        }
+//    }
+//
+//    override fun onGetUserSuccess(response: UserResponse) {
+//        dismissLoadingDialog()
+//        for (User in response.result) {
+//            Log.d("HomeFragment", User.toString())
+//        }
+//        binding.homeButtonTryGetJwt.text = response.message
+//        showCustomToast("Get JWT 성공")
+//    }
+//
+//    override fun onGetUserFailure(message: String) {
+//        dismissLoadingDialog()
+//        showCustomToast("오류 : $message")
+//    }
+//
+//    override fun onPostSignUpSuccess(response: SignUpResponse) {
+//        dismissLoadingDialog()
+//        binding.homeBtnTryPostHttpMethod.text = response.message
+//        response.message?.let { showCustomToast(it) }
+//    }
+//
+//    override fun onPostSignUpFailure(message: String) {
+//        dismissLoadingDialog()
+//        showCustomToast("오류 : $message")
     }
 }
